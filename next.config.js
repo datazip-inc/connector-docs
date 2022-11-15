e816fb96579d9eeee2635000def6a0e521eedc7d
ps-89 <ispratyaksh@gmail.com>
2022-11-15 10:39:11 +0530

    testing code highlighting

--- next.config.js
+++ next.config.js
@@ -1,19 +1,51 @@
-module.exports = ({
-  pageExtensions: ["tsx"],
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push(
-      ...[
-        {
-          test: /\.yml$/,
-          type: "json",
-          use: "yaml-loader",
-        },
-        {
-          test: /\.svg$/,
-          use: "@svgr/webpack",
-        },
-      ]
-    );
-    return config;
-  },
+const { remarkCodeHike } = require("@code-hike/mdx");
+// const theme = require("shiki/themes/github-dark.json"); // any theme from shiki
+const theme = require("shiki/themes/monokai.json");
+
+const withMDX = require("@next/mdx")({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [[remarkCodeHike, { theme }]],
+        rehypePlugins: [],
+    },
 });
+
+module.exports = withMDX({
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.module.rules.push(
+            ...[
+                {
+                    test: /\.yml$/,
+                    type: "json",
+                    use: "yaml-loader",
+                },
+                {
+                    test: /\.svg$/,
+                    use: "@svgr/webpack",
+                },
+            ]
+        );
+        return config;
+    },
+});
+
+// module.exports = ({
+//   pageExtensions: ["tsx"],
+//   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//     config.module.rules.push(
+//       ...[
+//         {
+//           test: /\.yml$/,
+//           type: "json",
+//           use: "yaml-loader",
+//         },
+//         {
+//           test: /\.svg$/,
+//           use: "@svgr/webpack",
+//         },
+//       ]
+//     );
+//     return config;
+//   },
+// });
